508dfb034fc69846624d8f65fba874371b6357d8
Arjun Vasan <arjun@imac.local>
2020-06-21 13:48:41 -0700

    a

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
-    let msg = new SpeechSynthesisUtterance("Welcome to CyborgOps");
-    msg.volume = 0.2;
-    msg.rate = 1.2;
-    msg.onend = (ev) => {
-    // do something
-    }
-    window.speechSynthesis.speak(msg);
+    $('#logo').click(function () {
+        let msg = new SpeechSynthesisUtterance("Welcome to CyborgOps");
+        msg.volume = 0.2;
+        msg.rate = 1.2;
+        msg.onend = (ev) => {
+        // do something
+        }
+        window.speechSynthesis.speak(msg);
+    })
 });
