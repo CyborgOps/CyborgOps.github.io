db3707977d3aa3f40244650a75677281625e2e4f
Arjun Vasan <arjun@imac.local>
2020-06-21 13:47:36 -0700

    f

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     let msg = new SpeechSynthesisUtterance("Welcome to CyborgOps");
     msg.volume = 0.2;
     msg.rate = 1.2;
-    msg.voice = getVoice();
     msg.onend = (ev) => {
     // do something
     }
